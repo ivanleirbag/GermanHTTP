2059717ab59270781f9c3f7ac6b8f9e9d20e6f08
ivanleirbag <ivanpeceto20@gmail.com>
2024-11-04 18:59:14 -0300

    Avances en la clase Car para los autos del juego

--- car.cpp
+++ car.cpp
@@ -1,3 +1,33 @@
 #include "car.h"
 
-Car::Car() {}
+
+Car::Car(QString& carImgDir, int carID, int initX, int initY)
+{
+    setImage(carImgDir);
+    ID = carID;
+    position.setX(initX);
+    position.setY(initY);
+}
+
+void Car::setImage(QString &carImgDir)
+{
+    QFile carImgFile;
+
+    carImgFile.setFileName(carImgDir);
+    carImgFile.open(QFile::ReadOnly);
+
+    image.clear();
+    image.append(carImgFile.readAll());
+
+    carImgFile.close();
+}
+
+QByteArray Car::getImage()
+{
+    return image;
+}
+
+void Car::updateCorners()
+{
+    corners
+}
--- car.h
+++ car.h
@@ -1,10 +1,59 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include "QString"
+#include <QImage>
+#include <QColor>
+#include "QFile"
+#include <vector>
+#include <QPoint>
+
 class Car
 {
 public:
-    Car();
+    //Public Vars
+    QPoint position;
+    std::vector<QPoint>corners;
+
+    //Public Funcs
+    /**
+     * @brief Car
+     * Car object constructor
+     * Loads the the image for the car and asigns an ID to it
+     * Asigns initial X and Y positions
+     * @param carImgDir
+     * Directory to the car image
+     * @param carID
+     * @param initX
+     * @param initY
+     */
+    Car(QString& carImgDir, int carID, int initX, int initY);
+
+    /**
+     * @brief getImage
+     * Car image getter
+     * @return
+     * Returns a QByteArray containing the car image
+     */
+    QByteArray getImage();
+
+    void updateCorners();
+
+private:
+    //Private Variables
+    QByteArray image;
+    int ID;
+
+    //Private Funcs
+    /**
+     * @brief setImage
+     * Car image setter
+     * @param carImgDir
+     * Directory to the car image
+     */
+    void setImage(QString& carImgDir);
+
+
 };
 
 #endif // CAR_H
